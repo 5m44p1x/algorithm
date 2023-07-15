0428394a7d9f11e5dc54a09dbf9c8841bd9264f4
5m44p1x <5m44p1x@gmail.com>
2023-07-15 14:52:43 -0600

    added selection sort and ignore file

---
+++ 1. Complexity/src/selectionSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_index;
+
+    for(i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        
+        if(min_index != i)
+        {
+            swap(arr[min_index], arr[i]);
+        }        
+    }
+}
+
+int main()
+{
+    int arr[] = {65, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
